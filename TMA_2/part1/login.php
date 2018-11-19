7ed1e3906115b1a0c590a974c33db32332eda0d9
Mosh333 <howladermoshiur@gmail.com>
2018-11-19 17:50:35 -0500

    Update login.php

--- TMA_2/part1/login.php
+++ TMA_2/part1/login.php
@@ -1,7 +1,11 @@
 <?php
-$servername = "db4free.net";
-$username = "mosh333";
-$password = "moshiur391";
+$servername = "db4free.net"; //"sql312.unaux.com";//
+$username = "mosh333"; //"unaux_23022042";//
+$password = "moshiur391";//"moshiur391";
+$database = "unaux_23022042_Comp466_Assign2_Part1";
+
+//how to find password
+//https://profreehost.com/forum/discussion/comment/2964/#Comment_2964
 
 // Create connection
 $conn = new mysqli($servername, $username, $password);
